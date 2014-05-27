05e311a6b083e91fd532f6da14185f63d2bb3ab9
nodiscc <nodiscc@gmail.com>
2014-05-27 18:25:54 +0200

    fun/3-random-words.sh: only output words, remove garbage

--- fun/3-random-words.sh
+++ fun/3-random-words.sh
@@ -4,5 +4,5 @@ sed `perl -e "print int rand(99999)"`"q;d" /usr/share/hunspell/en_US.dic >> chec
 }
 
 while [ `cat checkword 2>/dev/null | wc -l` -ne "3" ]; do checkword; done
-cat checkword
+cat checkword|awk -F '[/ \t]' '{print $1}'
 rm checkword
