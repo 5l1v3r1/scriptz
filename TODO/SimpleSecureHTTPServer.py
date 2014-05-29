e3ec59986ee1ff5672e5e6418c7290275c9e4c5e
nodiscc <nodiscc@gmail.com>
2014-05-30 01:06:55 +0200

    SimpleSecureHTTPServer.py: update from https://gist.github.com/ubershmekel/6194556, now uses python3

--- TODO/SimpleSecureHTTPServer.py
+++ TODO/SimpleSecureHTTPServer.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
-SimpleSecureHTTPServer.py - simple HTTP server supporting SSL.
+SimpleSecureHTTPServer.py - simple HTTP server supporting SSL/TLS. I.e. HTTPS. For python 3.3
+Source: https://gist.github.com/ubershmekel/6194556
 Thanks to http://code.activestate.com/recipes/442473/
+
 Needs python-openssl package on Debian.
 Manually generate the SSL certificate with:
 openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
-
-TODO: Bug with TypeError: shutdown() takes exactly 0 arguments (1 given)
 
 - replace fpem with the location of your .pem server file.
 - the default port is 4443.
@@ -14,39 +14,29 @@ TODO: Bug with TypeError: shutdown() takes exactly 0 arguments (1 given)
 usage: python SimpleSecureHTTPServer.py
 '''
 import socket, os
-from SocketServer import BaseServer
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from OpenSSL import SSL
+from socketserver import BaseServer
+from http.server import HTTPServer
+from http.server import SimpleHTTPRequestHandler
+import ssl
 
+CERT = 'server.pem'
+KEY = 'server.pem'
 
 class SecureHTTPServer(HTTPServer):
     def __init__(self, server_address, HandlerClass):
         BaseServer.__init__(self, server_address, HandlerClass)
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        #server.pem's location (containing the server private key and
-        #the server certificate).
-        fpem = 'server.pem'
-        ctx.use_privatekey_file (fpem)
-        ctx.use_certificate_file(fpem)
-        self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
-                                                        self.socket_type))
+        ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        ctx.load_cert_chain(certfile=CERT, keyfile=KEY)
+        self.socket = ctx.wrap_socket(socket.socket(self.address_family, self.socket_type), server_side=True)
         self.server_bind()
         self.server_activate()
 
-class SecureHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def setup(self):
-        self.connection = self.request
-        self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
-        self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
-
-
-def test(HandlerClass = SecureHTTPRequestHandler,
+def test(HandlerClass = SimpleHTTPRequestHandler,
          ServerClass = SecureHTTPServer):
     server_address = ('', 4443) # (address, port)
     httpd = ServerClass(server_address, HandlerClass)
     sa = httpd.socket.getsockname()
-    print "Serving HTTPS on", sa[0], "port", sa[1], "..."
+    print("Serving HTTPS on", sa[0], "port", sa[1], "...")
     httpd.serve_forever()
 
 
