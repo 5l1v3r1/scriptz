bc81b86ba590f4c968869cf9ab235f7a78575f2e
nodiscc <nodiscc@gmail.com>
2014-07-03 11:04:33 +0200

    working keyfile-creation-assistant.sh

--- sysadmin/keyfile-creation-assistant.sh
+++ sysadmin/keyfile-creation-assistant.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 #Keyfile creation wizard
-sudo dd if=/dev/urandom of=/root/keyfile bs=1024 count=4
-sudo chmod 0400 /root/keyfile
-sudo cryptsetup luksAddKey /dev/sdX /root/keyfile
-sudo nano /etc/crypttab #sdX_crypt /dev/sdX /root/keyfile luks
-sdX_crypt /dev/disk/by-uuid/247ad289-dbe5-4419-9965-e3cd30f0b080 /root/keyfile luks
+#Description: Create a LUKS key file, add it to the specified volume, set up automount for it
+#Must be run as root
+#Source: https://github.com/nodiscc/scriptz
+#License: MIT
+#TODO: add root check
+#TODO: specify targetdrive (or file) and mapper name in command line
+#TODO: add option to create luks volume before setting up the key
+
+#Vars
+set -e
+TargetDrive="/dev/sda"
+MapperName="cryptvolume"
+MountPoint="/media/$MapperName"
+Keyfile="/etc/keyfile-$MapperName"
+
+#Keyfile generation
+echo "Generating keyfile in $Keyfile..."
+sudo dd if=/dev/urandom of="$Keyfile" bs=1024 count=4
+sudo chmod 0400 "$Keyfile"
+
+#add keyfile to target drive
+sudo cryptsetup luksAddKey "$TargetDrive" "$Keyfile"
+echo "Keyfile added to target drive"
+
+#add entry to crypttab
+CrypttabLine="$MapperName $TargetDrive $Keyfile luks"
+echo -e  "\n $CrypttabLine" | tee -a /etc/crypttab
+echo "Entry added to crypttab"
+
+
+#add entry to fstab and mount
 sudo nano /etc/fstab
-/dev/mapper/sdX_crypt /media/sdX ext3 defaults 0 2
+FstabLine="/dev/mapper/$MapperName $MountPoint ext4 defaults 0 2"
+echo -e "\n $FstabLine" | tee -a /etc/fstab
+echo "Entry added to fstab"
 sudo mount -a
+echo "Drive mounted. Have a nice day."
 
 #detect luks partitions
 #for i in * ; do cryptsetup luksDump $i 2>/dev/null|grep UUID; done
