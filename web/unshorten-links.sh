54234fc0c310abf686f156d9537a901315e38b2d
nodiscc <nodiscc@gmail.com>
2014-08-09 20:34:28 +0200

    fix unshorten-links.sh, now working with new API

--- web/unshorten-links.sh
+++ web/unshorten-links.sh
@@ -3,7 +3,11 @@
 #License: MIT (http://opensource.org/licenses/MIT)
 #Source: https://github.com/nodiscc/scriptz
 
-LONGURL=`curl -s http://www.unshorten.it/api1.0.php?shortURL=$1`
+if [ -z "$1" ]
+	then echo "USAGE: $(basename $0) SHORT_URL"; exit 1
+fi
+
+LONGURL=`curl -s http://api.unshorten.it?shortURL=$1\&apiKey=Injgj4PqDf4AcikibfgjAUN9yq9YlseP`
 #If there was an error, return the original url
 if [[  "$LONGURL" == *error* ]]
 then
