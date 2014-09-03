b6098cbad9c13e1eb8f29e3d750da2cc098c40ae
nodiscc <nodiscc@gmail.com>
2014-09-04 00:27:50 +0200

    luks-helper.sh: new options, run as root:  * check for root at start  * remove sudo calls  * add -t option (generate random key for new volume)  * add -f option (foreground, close volume on user input)

--- sysadmin/luks-helper.sh
+++ sysadmin/luks-helper.sh
@@ -10,35 +10,44 @@ R="\033[01;31m"
 NC="\033[00m"
 
 #Usage
-USAGE="$(basename $0) -a action -c container [-s size] [-m mountpoint] [-t]
+USAGE="$(basename $0) -a action -c container [-s size] [-m mountpoint] [-t] [-f]
 	action: 	mount,unmount,create
 	container:	LUKS container filename
 	mountpoint:	where to mount the volume
 	size:		size in MB for new volumes
-	-t:		use a randomly generated 30-character password"
+	-t:		use a randomly generated 30-character password for volume creation
+	-f		foreground mode (for mount action only - stay in the foreground and press enter to unmount)"
 
+##########################################################################
+
+_CheckRoot() {
+	if [ $(whoami) != "root" ]
+		then echo "You must have root to run this script."
+		exit 1
+	fi
+}
 
 _CreateVolume() { #Create and format a LUKS volume
 	if [ -z "$size" ]
 		then echo -e "${R}No size defined for new volume.${NC}"; exit 1
 	fi
 
-	echo -e "${G}Writing $size MB zeroes to $container${NC}"
-	dd if=/dev/zero bs=1M count="$size" of="$container"
+	echo -e "${G}Writing $size MB random data to $container${NC}"
+	dd if=/dev/urandom bs=1M count="$size" of="$container"
 	if [ "$TEMPKEY" != "1" ]
 		then
 			echo -e "${G}Formatting $container as LUKS volume${NC}"
-			sudo cryptsetup luksFormat "$container"
+			cryptsetup luksFormat "$container"
 		else
 			pwgen=$(pwgen -s 30 -n 1)
 			echo -e "${G}Formatting $container as LUKS volume, key is $pwgen${NC}"
-			echo "$pwgen" | sudo cryptsetup luksFormat "$container"
+			echo "$pwgen" | cryptsetup luksFormat "$container"
 	fi
 
 	echo -e "${G}Creating ext4 partition inside LUKS volume${NC}"
-	sudo cryptsetup luksOpen "$container" $(basename "$container")
-	sudo mkfs.ext4 /dev/mapper/$(basename "$container")
-	sudo cryptsetup luksClose $(basename "$container")
+	cryptsetup luksOpen "$container" $(basename "$container")
+	mkfs.ext4 /dev/mapper/$(basename "$container")
+	cryptsetup luksClose $(basename "$container")
 }
 
 
@@ -50,22 +59,32 @@ _MountVolume() { #Unlock and mount a LUKS volume
 	fi
 
 	echo -e "${G}Mounting $container on $mountpoint${NC}".
-	sudo cryptsetup luksOpen "$container" $(basename "$container")
-	sudo mount /dev/mapper/$(basename "$container") "$mountpoint"
+	cryptsetup luksOpen "$container" $(basename "$container")
+	mount /dev/mapper/$(basename "$container") "$mountpoint"
+
+	if [ "$FOREGROUND" == 1 ]
+		then echo -n "Press Enter to unmount volume."
+		read
+		_UnmountVolume
+	fi
 }
 
 
 _UnmountVolume() { #Unmount and lock a LUKS volume
 	mappername="/dev/mapper/$(basename $container)"
 	echo -e "${G}Unmounting $mappername${NC}"
-	sudo umount "$mappername"
+	umount "$mappername"
 	echo -e "${G}Closing $container LUKS volume${NC}"
-	sudo cryptsetup luksClose $(basename "$container")
+	cryptsetup luksClose $(basename "$container")
 }
 
+#########################################################################
 #Main loop
-#Parse options
-while getopts ":a:c:s:m:th" opt; do
+
+
+_CheckRoot
+
+while getopts ":a:c:s:m:tfh" opt; do
 	case $opt in
 	a) action=$OPTARG
 	;;
@@ -79,20 +98,23 @@ while getopts ":a:c:s:m:th" opt; do
 	;;
 	h) echo "$USAGE"; exit 0
 	;;
+	f) FOREGROUND="1"
+	;;
 	*) echo -e "${R}Invalid option $opt${NC}"; echo "$USAGE"; exit 1
 	;;
 	esac
 done
 
 
-#Checks for mandatory options
+
 if [ -z "$action" ]
 	then echo -e "${R}No action specified${NC}"; echo "$USAGE"; exit 1
 elif [ -z "$container" ]
 	then echo -e "${R}No container specified${NC}"; echo "$USAGE"; exit 1
 fi
 
-#Select action
+
+
 case $action in
 	"create" ) _CreateVolume ;;
 	"mount" ) _MountVolume ;;
