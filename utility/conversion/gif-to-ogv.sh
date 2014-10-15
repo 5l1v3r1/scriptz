0b2366137daa4ef00f3c3f1d030eb4ca46f75922
nodiscc <nodiscc@gmail.com>
2014-10-15 15:06:51 +0200

    cleanup: gif-to-ogv: use getopts

--- utility/conversion/gif-to-ogv.sh
+++ utility/conversion/gif-to-ogv.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 #Description: Convert animated gifs to ogg video files
-#Source: https://github.com/nodiscc/scriptz
+#Copyright: (c) 2014 nodiscc <nodiscc@gmail.com>
 #License: MIT (http://opensource.org/licenses/MIT)
 #Dependencies: avconv, mplayer
 #Saves a lot of space without sacrificing quality. Plays nice with mplayer -loop 0 output.ogv 
 #TODO: make the output cleaner/more informative
+#TODO: add compatibility for ffmpeg
 set -e
 
 
-USAGE="USAGE: `basename $0` [ -d ] file1.gif file2.gif ...
-OPTIONS:    -d    delete original after conversion"
+USAGE="$(basename $0) [-d] file1.gif file2.gif file3.gif ...
+	-d:	delete original after conversion"
+
+while getopts ":d" opt; do
+	case $opt in
+	d) DELETE_ORIGINAL="true"; shift
+	;;
+	h) echo "$USAGE"; exit 0
+	;;
+	*) echo -e "${R}Invalid option $opt${NC}"; echo "$USAGE"; exit 1
+	;;
+	esac
+done
 
 
-if [ "$1" = "-d" ]
-then DELETE_ORIGINAL="true"; shift
-elif [ "$1" = "-h" -o "$1" = "--help" -o "$1" = "" ]
-then echo "$USAGE"; exit 0
+if [ $1 = "" ]
+	then echo "$USAGE"; exit 1
 fi
 
 for file in $@
