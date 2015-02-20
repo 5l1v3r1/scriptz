7741c7d20574489495c042bc0b5d2f7009325e6d
nodiscc <nodiscc@gmail.com>
2015-02-20 19:28:11 +0100

    userscripts: colroful reddit usernames: use https

--- web/userstyles-userscripts/Colorfull_reddit_usernames_(CRU)/Colorfull_reddit_usernames_(CRU).user.js
+++ web/userstyles-userscripts/Colorfull_reddit_usernames_(CRU)/Colorfull_reddit_usernames_(CRU).user.js
@@ -5,8 +5,8 @@
 // @namespace       sxxe@gmx.de
 // @grant           GM_config
 // @grant           GM_registerMenuCommand
-// @include         http://*.reddit.com/r/*/comments/*
-// @require         http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @include         https://*.reddit.com/r/*/comments/*
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require         https://greasyfork.org/libraries/GM_config/20131122/GM_config.js
 
 // ==/UserScript==
