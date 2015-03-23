5a5117103bb822c02328a29d7389d4277f720c06
nodiscc <nodiscc@gmail.com>
2015-03-23 21:17:12 +0100

    add dev/git/git-add-all-github-forks.sh  * addw all forks for a github project as git remotes

---
+++ dev/git/git-add-all-github-forks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Description: add all forks for a github project as git remotes
+#Copyright (c) 2015 nodiscc <nodiscc@gmail.com>
+#License: MIT (http://opensource.org/licenses/MIT)
+#Requires: curl, jq
+
+usage="$0 owner repo"
+owner="$1"
+repo="$2"
+
+if [ "$1" = "" -o "$2" = "" ]
+    then echo "$USAGE"
+fi
+
+data=$(curl https://api.github.com/repos/$owner/$repo/forks)
+forks=$(echo "$data" |jq '.[] | {clone_url: .clone_url}' | \
+        egrep -oh "[A-Za-z]*/$repo\.git[^/]")
+
+echo "Forks found:"
+echo "$forks"
+
+users=$(echo "$forks" | cut -d"/" -f 1)
+echo "$users"
+
+for user in $users;
+    do
+    true
+    git remote add "$user" "https://github.com/$user/$repo.git"
+done
+
+git remote update
